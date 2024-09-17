f01ee23533818726d8d40ab68d7eeefb4a7c89fc
MidnightCrowing <110297461+MidnightCrowing@users.noreply.github.com>
2024-09-17 22:47:45 +0800

    fix(ui): override the changeEvent method of MSFluentFramelessWindow #2

--- src/app/window/frameless_window.py
+++ src/app/window/frameless_window.py
@@ -1,10 +1,42 @@
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QEvent
 from qfluentwidgets import MSFluentWindow
 
 
 class MSFluentFramelessWindow(MSFluentWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.pre_maximized_size = None  # 记录最大化前的窗口大小
+        self.is_restoring = False  # 标志用于恢复窗口状态
+
+    def changeEvent(self, event):
+        if event.type() == QEvent.WindowStateChange:
+            current_state = self.windowState()
+            previous_state = event.oldState()
+
+            if current_state & Qt.WindowMaximized:
+                # 窗口当前处于最大化状态
+                if self.pre_maximized_size is None:
+                    # 记录最大化前的窗口大小
+                    self.pre_maximized_size = self.size()
+
+                if previous_state & Qt.WindowMinimized:
+                    # 窗口从最小化状态恢复
+                    self.is_restoring = True
+                    self.adjustSize()  # 调整窗口大小
+                    self.showMaximized()  # 恢复最大化
+
+            elif previous_state & Qt.WindowMaximized:
+                # 窗口从最大化状态恢复到普通状态
+                if self.is_restoring:
+                    # 恢复最大化状态时设置的标志
+                    self.is_restoring = False
+                else:
+                    # 恢复到最大化前的大小
+                    if self.pre_maximized_size is not None:
+                        self.resize(self.pre_maximized_size)
+                        self.pre_maximized_size = None  # 清除记录的大小
+
+        super().changeEvent(event)
 
     def updateFrameless(self):
         # 覆盖父类WindowsFramelessWindowBase方法，修复vtk渲染器背景颜色问题
