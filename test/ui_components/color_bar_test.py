fcb135fb4db3b0d384c76f5378a383e6d18198cd
MidnightCrowing <110297461+MidnightCrowing@users.noreply.github.com>
2024-09-20 14:40:43 +0800

    refactor(test): delete redundant code of color bar test

--- test/ui_components/color_bar_test.py
+++ test/ui_components/color_bar_test.py
@@ -6,10 +6,8 @@ print("修改后工作目录:", os.getcwd())
 
 import sys
 
-import cv2
 from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget
-from qfluentwidgets import ToolTipPosition, ToolTipFilter, isDarkTheme, PushButton
-from sklearn.cluster import KMeans
+from qfluentwidgets import ToolTipPosition, ToolTipFilter, isDarkTheme
 
 from src.app.components import ColorBar
 # noinspection PyUnresolvedReferences
@@ -32,12 +30,6 @@ class ColorBarTest(QWidget):
 
         self.show_dominant_colors()
 
-    def load_image(self):
-        """Load and store the image"""
-        image = cv2.imread(image_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        self.image = image
-
     def show_dominant_colors(self):
         """Extract and display dominant colors in the ColorBar"""
         dominant_colors = [(112, 82, 89),
@@ -47,15 +39,6 @@ class ColorBarTest(QWidget):
                            (16, 48, 115)]
         self.color_bar.setColors(dominant_colors)
 
-    def get_dominant_colors(self, image, k=5):
-        """Use KMeans to find the dominant colors in the image"""
-        pixels = image.reshape((-1, 3))
-        kmeans = KMeans(n_clusters=k)
-        kmeans.fit(pixels)
-        return kmeans.cluster_centers_.astype(int)
-
-
-image_path = r'C:\Users\lenovo\Pictures\b_2feb4198a9d76db5ff777cdd1b4e9cb6.jpg'
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
